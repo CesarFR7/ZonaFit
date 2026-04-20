dd2a2d00e3b9946b12450b62f2af99ccbb3a6252
Cesar Flores <136465@udlondres.com>
2026-04-20 00:20:33 -0600

    Creación del template index html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+titulo_app = "Zona Fit (GYM)"
 
 
 @app.route("/")  # url: http://127.0.0.1:5000
 def inicio():
     app.logger.debug("Entramos al path de inicio /")
-    return "<p>Inicio del proyecto</p>"
+    return render_template("index.html", titulo=titulo_app)
 
 
 if __name__ == "__main__":
