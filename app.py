35498b2693f438cf5477e6dbacab128115c3f69f
Cesar Flores <136465@udlondres.com>
2026-04-23 16:53:19 -0600

    se creó un objeto cliente form vacío en app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from cliente_dao import ClienteDAO
+from cliente import Cliente
+from cliente_forma import ClienteForma
 
 app = Flask(__name__)
 
@@ -8,10 +10,15 @@ titulo_app = "Zona Fit (GYM)"
 
 @app.route("/")  # url: http://127.0.0.1:5000
 def inicio():
+    app.logger.debug("Entramos al path de inicio /")
     # Recuperamos los clientes de la BD
     clientes_db = ClienteDAO.seleccionar()
-    app.logger.debug("Entramos al path de inicio /")
-    return render_template("index.html", titulo=titulo_app, clientes=clientes_db)
+    # Creamos un objeto de cliente form vacío
+    cliente = Cliente()
+    cliente_form = ClienteForma(obj=cliente)
+    return render_template(
+        "index.html", titulo=titulo_app, clientes=clientes_db, forma=cliente_form
+    )
 
 
 if __name__ == "__main__":
