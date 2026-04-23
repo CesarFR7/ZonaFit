ea8eaa82ba1b982b74624a9a4a420a9e14f93e1d
Cesar Flores <136465@udlondres.com>
2026-04-23 14:21:32 -0600

    Recuperamos los datos de la BD
    Se utiliza la clase ClienteDAO para obtener la lista de los clientes y se comparten al template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from cliente_dao import ClienteDAO
 
 app = Flask(__name__)
 
@@ -7,8 +8,10 @@ titulo_app = "Zona Fit (GYM)"
 
 @app.route("/")  # url: http://127.0.0.1:5000
 def inicio():
+    # Recuperamos los clientes de la BD
+    clientes_db = ClienteDAO.seleccionar()
     app.logger.debug("Entramos al path de inicio /")
-    return render_template("index.html", titulo=titulo_app)
+    return render_template("index.html", titulo=titulo_app, clientes=clientes_db)
 
 
 if __name__ == "__main__":
